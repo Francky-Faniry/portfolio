7172cc4b7637d87d0638880369c36907791297dc
Francky <kyfr.rakotonirina1@gmail.com>
2025-06-17 20:29:09 +0200

    section Apropos

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,31 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div id="about-me">
+    <h2 class="text-highlight-1 section-title">Qui suis-je ?</h2>
+
+    <p class="default-text">
+        <span class="text-highlight-2">Développeur web</span>, plutôt spécialisé
+        <span class="text-highlight-2">Back-end</span> (mais la partie <span class="text-highlight-2">Front-end</span>
+        ne me fait pas peur). <br/>
+        Mon Framework Php de prédilection est <span class="text-highlight-2">Laravel</span> .
+        Côté Js, j'utilise <span class="text-highlight-2">Vue JS</span> . <br/>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+    #about-me {
+        margin-top: 80px;
+    }
+
+    .default-text {
+        text-align: justify;
+    }
+
+    .default-text span {
+        font-weight: bold;
+    }
+</style>
